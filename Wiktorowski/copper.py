9ea59f840fd496b7ef5aa88065b3d29530a0f8ee
Michał <86123830+mweuro@users.noreply.github.com>
2023-04-05 09:35:43 +0200

    dałem jupyter notebook i tam teraz piszę

--- Wiktorowski/copper.py
+++ Wiktorowski/copper.py
@@ -1,4 +1,3 @@
-import pyexpat
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,22 +15,16 @@ copper = pd.read_csv('copper.csv', sep = ',', encoding = 'latin-1')
 # plt.plot(copper['Date'].values[::-1], copper['Open'].values[::-1])
 # plt.show()
 
+# Histogram logarytmicznych przyrostów
 s = np.array(copper['Open'].values[1:])
 s2 = np.array(copper['Open'].values[:-1])
 r = np.log(s/s2)
 plt.hist(r, bins = 30, density = True)
-plt.show()
-
-# WOLFRAM
-tungstum = pd.read_csv('tungstum.csv', sep = ',')
-
-## # Ceny akcji miedzi
-# plt.plot(tungstum['Date'].values[::-1], tungstum['Open'].values[::-1])
 # plt.show()
 
-ss = np.array([float(i[1:]) for i in tungstum['Open'].values[1:]])
-ss2 = np.array([float(i[1:]) for i in tungstum['Open'].values[:-1]])
+# Kwantyle
+print(np.arange(0, 1, 0.01))
+# def draw_quantile():
+#     alpha = np.arange()
 
-p = np.log(ss/ss2)
-plt.hist(p, bins = 30, density = True)
-plt.show()
+##################################################################
